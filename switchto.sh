16e83d097a1260dbc945187096185903aa533cda
Mariam Wagdy <mariam.wagdy92@gmail.com>
2022-07-10 15:50:55 +0200

    fix bug

--- switchto.sh
+++ switchto.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-	
+   
+	function getCurrentVersion(){
+		CURRENT_PHP_VERSION="$(php -r "echo PHP_MAJOR_VERSION; echo '.'; echo PHP_MINOR_VERSION;")"
+	}
+
 	if [ -z "$1" ]
 	  then
 	    echo "Error: no version given"
+		getCurrentVersion
+	    echo ${CURRENT_PHP_VERSION}" process is running."
+	    exit 0
 	fi
 
-    CURRENT_PHP_VERSION="$(php -r "echo PHP_MAJOR_VERSION; echo '.'; echo PHP_MINOR_VERSION;")"
+	TO_PHP_VERSION="$1"
 
 	if [ ! -d "/etc/php/$TO_PHP_VERSION" ]; then
 	    echo "$TO_PHP_VERSION Version is not available!"
+	    getCurrentVersion
 	    echo ${CURRENT_PHP_VERSION}" process is running."
 	    exit 0
 	fi
@@ -29,6 +37,7 @@
  	servstat=$(sudo service apache2 status)
 
 	if [[ $servstat == *"active (running)"* ]]; then
+		getCurrentVersion
 	  	echo "php ${CURRENT_PHP_VERSION} is running. enjoy!"
 	else 
 		echo "apache is not running."
